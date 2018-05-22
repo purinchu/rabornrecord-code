94c5a83192c37b3b52d9f9cc7efa81e222189283
Michael Pyne <mpyne@purinchu.net>
2018-05-22 00:13:02 -0400

    Manually setup node.js app and run it.
    Since most of the GCP documentation implies this type of deployment for
    the flexible AppEngine runtime, that way I'm hopefully less confused by
    this later.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-exports.getRecord = (req, res) => {
-    // Real short record for now...
-    res.send('{"name": "Foo"}');
-};
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  // Real short record for now...
+  res.status(200).send('{"name": "Foo"}');
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
